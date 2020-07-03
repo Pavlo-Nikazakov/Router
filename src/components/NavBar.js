da3f066de82feff68fab99c3f9c8ac1d7c241f39
Pavlo-Nikazakov <pavlo.nikazakov@gmail.com>
2020-07-03 13:44:57 +0200

    fixed link import in NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <div>
       <li>
-        <NavLink exact={true} to="/HomePage">
-          Home Page
-        </NavLink>
+        <NavLink to="/HomePage">Home Page</NavLink>
       </li>
       <li>
         <NavLink to="/AboutPage">About ths Website</NavLink>
